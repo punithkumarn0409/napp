fa0fc5c2dbdc1ed730caa9ac9ba49f35280c2546
Punith <punithkumarn0409@gmail.com>
2025-02-22 02:47:33 +0530

    home page layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,41 +30,93 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Expanded(
-                flex: 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: EdgeInsets.symmetric(horizontal: width*0.04),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.white,
+                flex: 3,
+                  child: Card(
+                    elevation: 0,
+                    margin: EdgeInsets.symmetric(horizontal: width*0.02),
+                    color: Colors.deepPurple,
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            flex: 7,
+                            child: Container(
+                              margin: EdgeInsets.only(left: width*0.02),
+                              child: Center(
+                                child: Text('Preferences',
+                                  style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),),
+                              ),
+                            ),
                           ),
-                      ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.white,
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              margin: EdgeInsets.symmetric(horizontal: width*0.01),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Text('Age',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 18,
+                                ),),
+                              ),
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          margin: EdgeInsets.symmetric(horizontal: width*0.04),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.white,
                           ),
-                        ),
+                          Expanded(
+                            flex:6,
+                            child: Container(
+                              margin: EdgeInsets.symmetric(horizontal: width*0.01),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Text('Location',
+                                  style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 18,
+                                  ),),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 5,
+                            child: Container(
+                              margin: EdgeInsets.only(left: width*0.01, right: width*0.02),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Text('Gender',
+                                  style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 18,
+                                  ),),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
               ),
               Expanded(
-                flex: 24,
+                flex: 27,
                 child: Container(
                   margin: EdgeInsets.all(width*0.04),
                   decoration: BoxDecoration(
@@ -78,13 +130,35 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   children: [
                     Expanded(
-                      flex: 3,
+                      flex: 5,
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: width*0.01, horizontal: width*0.07),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(100),
                           color: Color(0xffc670fe),
                         ),
+                      child: Center(
+                      child: Row(
+                      children: [
+                      Container(
+                      margin: EdgeInsets.only(left:18),
+                        width: 40,
+                        child: Image.asset('assets/images/cross-small.png', color: Colors.white)
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left:3),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
                       ),
                     ),
                     Expanded(
@@ -93,12 +167,40 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     Expanded(
-                      flex: 5,
+                      flex: 6,
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: width*0.01, horizontal: width*0.07),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(100),
-                          color: Color(0xffba53fd),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomLeft,
+                              colors: [
+                                Color(0xff9b00ff),
+                                Color(0xffba53fd)
+                              ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left:30),
+                                width: 30,
+                                  child: Image.asset('assets/images/star.png', color: Colors.white)
+                                ),
+                              Container(
+                                margin: EdgeInsets.only(left:9),
+                                child: Text(
+                                  'Match',
+                                   style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                                                            ),
+                              ),
+                          ]
+                          ),
                         ),
                       ),
                     ),
@@ -115,8 +217,8 @@ class _HomePageState extends State<HomePage> {
           items: [
             BottomNavigationBarItem(
                 icon: Container(
-                  width: 40,
-                  margin: EdgeInsets.only(top: 15),
+                  width: 35,
+                  margin: EdgeInsets.only(top: 13),
                   child: Image.asset(
                     'assets/images/user.png',
                     color: Colors.white,
@@ -125,8 +227,8 @@ class _HomePageState extends State<HomePage> {
                 label: ''),
             BottomNavigationBarItem(
                 icon: Container(
-                  width: 40,
-                  margin: EdgeInsets.only(top: 15),
+                  width: 35,
+                  margin: EdgeInsets.only(top: 13),
                   child: Image.asset(
                     'assets/images/heart.png',
                     color: Colors.white,
@@ -135,8 +237,8 @@ class _HomePageState extends State<HomePage> {
                 label: ''),
             BottomNavigationBarItem(
                 icon: Container(
-                  width: 40,
-                  margin: EdgeInsets.only(top: 15),
+                  width: 35,
+                  margin: EdgeInsets.only(top: 13),
                   child: Image.asset(
                     'assets/images/message.png',
                     color: Colors.white,
